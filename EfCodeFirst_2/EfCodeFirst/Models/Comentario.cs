c41915ee6f3196f397b5c371892350dce3e84e75
virlero <34726006+virlero@users.noreply.github.com>
2018-06-07 00:46:13 +0200

    06-06-18

---
+++ EfCodeFirst_2/EfCodeFirst/Models/Comentario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EfCodeFirst.Models
+{
+    //L33c7a creamos nueva clase relacionada con BlogPost
+    public class Comentario
+    {
+        public int Id { get; set; }
+        public string Contenido { get; set; }
+        public string Autor { get; set; }
+        public int BlogPostId { get; set; }//clave foranea
+        [ForeignKey("BlogPostId")]
+        public  BlogPost BlogPost { get; set; } //Agremamos unha propiedad navegacional, desde un comentario
+                                                //podemos obter todas a propiedades do blogpost que lle corresponde
+    }
+}
